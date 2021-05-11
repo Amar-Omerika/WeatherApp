ec012a1b9b76e9a8fb77685d8e997abc29cb3327
Amar Omerika <amar.omerika@edu.fit.ba>
2021-05-11 16:37:16 +0200

    WeatherApp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { weather } from "./api/weather";
 
 const App = () => {
-	return <h1>Proba</h1>;
+	const [query, setQuery] = useState("");
+	const [sweather, setWeather] = useState("");
+
+	const search = async (ev) => {
+		if (ev.key == "Enter") {
+			const data = await weather(query);
+			setWeather(data);
+			setQuery("");
+		}
+	};
+
+	return (
+		<div>
+			<input
+				type="text"
+				className="search"
+				placeholder="Search.."
+				value={query}
+				onChange={(ev) => setQuery(event.target.value)}
+				onKeyPress={search}
+			/>
+		</div>
+	);
 };
 
 export default App;
--- src/api/weather.js
+++ src/api/weather.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const URL = "https://openweathermap.org/data/2.5/weather";
 const API_Key = "848db343e4019df22bfc3b1ef8d33439";
-const weathercall = async (query) => {
+const weather = async (query) => {
 	const { data } = await axios.get(URL, {
 		parameters: {
 			q: query,
@@ -12,3 +12,4 @@ const weathercall = async (query) => {
 	});
 	return data;
 };
+export default weather;
