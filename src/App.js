71950cb43e0d29e8daf391f5573d6c560f430258
Amar Omerika <amar.omerika@edu.fit.ba>
2021-05-11 16:52:55 +0200

    WeatherApp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
-import { fweather } from "./api/weather";
+
+import { fetchWeather } from "./api/fetchWeather";
+import "./App.css";
 
 const App = () => {
 	const [query, setQuery] = useState("");
-	const [sweather, setWeather] = useState("");
+	const [weather, setWeather] = useState({});
 
-	const search = async (ev) => {
-		if (ev.key == "Enter") {
-			const data = await weather(query);
+	const search = async (e) => {
+		if (e.key === "Enter") {
+			const data = await fetchWeather(query);
+
 			setWeather(data);
 			setQuery("");
 		}
@@ -18,9 +21,9 @@ const App = () => {
 			<input
 				type="text"
 				className="search"
-				placeholder="Search.."
+				placeholder="Search..."
 				value={query}
-				onChange={(ev) => setQuery(ev.target.value)}
+				onChange={(e) => setQuery(e.target.value)}
 				onKeyPress={search}
 			/>
 			{weather.main && (
@@ -29,6 +32,18 @@ const App = () => {
 						<span>{weather.name}</span>
 						<sup>{weather.sys.country}</sup>
 					</h2>
+					<div className="city-temp">
+						{Math.round(weather.main.temp)}
+						<sup>&deg;C</sup>
+					</div>
+					<div className="info">
+						<img
+							className="city-icon"
+							src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+							alt={weather.weather[0].description}
+						/>
+						<p>{weather.weather[0].description}</p>
+					</div>
 				</div>
 			)}
 		</div>
